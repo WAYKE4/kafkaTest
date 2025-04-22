8a6407cf42b47ebf0604203f5a93b650aab20ee3
WAYKE4 <gitta.tacrhi@mail.ru>
2025-04-23 00:02:09 +0300

    Add Consumer (main)

--- src/main/java/com/kafka/kafkatest/KafkaTestApplication.java
+++ src/main/java/com/kafka/kafkatest/KafkaTestApplication.java
@@ -1,15 +1,39 @@
 package com.kafka.kafkatest;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 @SpringBootApplication
 public class KafkaTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaTestApplication.class, args);
-    }
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Collections.singleton("demo-topic1"));
 
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Key " + record.key() + ", value: " + record.value());
+            }
+        }
+    }
 }
+
+//TODO:: controller , @kafkaListener
